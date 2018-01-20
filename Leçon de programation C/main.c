de0e9e2df6ede4d68723da9352856581e2f26226
Sacha Ryszfeld <sacha@MacBook-Pro-de-Sacha.local>
2018-01-20 01:06:23 +0100

    Initial Commit

---
+++ Leçon de programation C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Leçon de programation C
+//
+//  Created by Sacha Ryszfeld on 20/01/2018.
+//  Copyright © 2018 Sacha Ryszfeld. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
